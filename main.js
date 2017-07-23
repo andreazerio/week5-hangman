a523c9e197f47206ff411b55d2a11978929ff699
andreazerio <andrea.zerio@gmail.com>
2017-07-23 19:24:58 +0100

    added blanks list

--- main.js
+++ main.js
@@ -8,4 +8,13 @@ $(document).ready(function (){
       for (var i = 0; i <alphabet.length; i++) {
         $('#letList').append('<li class ="letC">'+alphabet[i]+'</li');
       }
+
+var words = ['ruler','laptop','wolf','lamp','serendipity','keyboard','mouse','glass','stand','book','radio','frequency','wave','paper','document','digit','wood','armchair','number'];
+var word = words[Math.floor(Math.random()*words.length)];
+$('#guesses').append('<ul id="guessList"></ul>');
+for (var i = 0; i < word.length; i++) {
+  var guess = $('<li></li>').html('_').attr('class','guessC').attr('id',i);
+  $('#guessList').append(guess);
+}
+
 });
